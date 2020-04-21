09d2af2d42ffa0c325dc619d40aadf4095b69629
Kartheek <kartheek@vebbler.com>
2020-04-21 19:01:33 +0530

    committed with project files

---
+++ SampleTableView/SampleTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleTableView
+//
+//  Created by Vebbler Technologies on 21/04/20.
+//  Copyright © 2020 Kartheek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
